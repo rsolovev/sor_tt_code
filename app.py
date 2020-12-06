69f0add06be4b944e59831903c95b22e88d91e36
Roman Solovev <r.solovev@innopolis.ru>
2020-12-06 14:33:37 +0300

    fix code, + keep static folders

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
+from datetime import datetime
+
 from flask import Flask
+import pytz
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
-    return 'Hello World!'
+    tz = pytz.timezone('Europe/Moscow')
+    msc_now = datetime.now(tz)
+    return "Current Moscow time is " + str(msc_now.strftime("%b %d %Y %H:%M:%S")) + "\n"
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=80)
